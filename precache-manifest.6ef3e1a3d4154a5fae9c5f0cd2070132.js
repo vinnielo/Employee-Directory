755478108a2ec8a61319d1a60799d6fe460d9397
jose Lopez <vinnielo01@gmail.com>
2020-06-21 15:27:12 -0700

    Updates

---
+++ precache-manifest.6ef3e1a3d4154a5fae9c5f0cd2070132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d056d24fc73e25aa3c72c07d28eb437",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "7e85de8a2d26e8f16f09",
+    "url": "/Employee-Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2369265034a4f802d7b",
+    "url": "/Employee-Directory/static/js/2.54b94837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.54b94837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e85de8a2d26e8f16f09",
+    "url": "/Employee-Directory/static/js/main.f1c7cc28.chunk.js"
+  },
+  {
+    "revision": "6da714a2be1f0ed36525",
+    "url": "/Employee-Directory/static/js/runtime-main.32c57308.js"
+  }
+]);
